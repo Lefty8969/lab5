19dd68bbc8a7299c305b401671077d5d25d12681
Lou Escobar <luisbermeo18@yahoo.com>
2023-10-28 02:20:03 -0400

    adds

--- js/app.js
+++ js/app.js
@@ -28,12 +28,7 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
     let sumAnswer = sum(a,b);
-  sumAnswer=sum(sumAnswer[0], c);
-  let multAnswer = multiply(a,b);
-  multAnswer = multiply(multAnswer[0],c);
-  let sumString = `${a} and ${b} and ${c} sum to ${sumAnswer[0]}.`;
-  let multString = `The product of ${a} and ${b} and ${c} is ${multAnswer[0]}.`;
-  return [sumAnswer[0],multAnswer[0], sumString, multString ];
+
 
 
 }
@@ -57,10 +52,16 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
 
+  sumAnswer=sum(sumAnswer[0], c);
+  let multAnswer = multiply(a,b);
+  multAnswer = multiply(multAnswer[0],c);
+  let sumString = `${a} and ${b} and ${c} sum to ${sumAnswer[0]}.`;
+  let multString = `The product of ${a} and ${b} and ${c} is ${multAnswer[0]}.`;
+  return [sumAnswer[0],multAnswer[0], sumString, multString ];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+ testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
